f7981c6ba94fb5b333f57785ff78a63f6c08b1c8
Alex Scott <alex.scott@tipsforthings.com>
2016-04-12 22:35:26 +0100

    Fix PHP templating and remove unused files.

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+
+    <script src="js/vendor/jquery.min.js"></script>
+    <script src="js/vendor/what-input.min.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script src="js/app.js"></script>
+
+  </body>
+</html>
